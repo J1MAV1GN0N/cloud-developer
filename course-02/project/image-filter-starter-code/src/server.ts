4e8ac308757e509ff3514113ef0230fc3cc94582
Philipp Christ <philipp.christ.1997@gmail.com>
2020-12-14 16:29:59 +0100

    project server TODOs

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,9 @@
 import express, { response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {
+  filterImageFromURL,
+  deleteLocalFiles
+} from './util/util';
 
 (async () => {
 
@@ -13,56 +16,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-/*
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  app.get("/filteredimage", async ( req, res ) => {
-  //  1. validate the image_url query
-    let exImage;
-    let filteredImagePath;
-    try {
-      // Is there an existent image? True || False?
-      exImage = await imageExists(req.query.image_url);
-
-  //  2. call filterImageFromURL(image_url) to filter the image
-      if (exImage) {
-        filteredImagePath = await filterImageFromURL(req.query.image_url)
-        res.status(200).sendFile(filteredImagePath);
-      } else {
-        return res.status(415).send("Image type is not supported.")
-      }
-    }
-    catch (error) {
-      response.status(400).send(error);
-    }
-  });
-
-//    3. send the resulting file in the response (upload filtered image file).
-  app.post("/filter-image", upload.single("file"), async (req, res) => {
-    if (!req.file || !isSupportedFileExt(path.extname(req.file.originalname))) {
-      return res
-        .status(403)
-        .contentType("text/plain")
-        .end("This image is not supported.");
-    }
-
-    const filteredImagePath = await filterImageFromURL(
-      // Note: I'll ignore tslint warning of no trailing comma,
-      // as trailing comma here is removed by prettier formatter.
-      // See https://prettier.io/docs/en/options.html
-      path.join(appRoot.path, req.file.path)
-    );
-    res.status(200).sendFile(filteredImagePath);
-  });
-
-
-  /* Delete image files on server. 
-  app.post("/delete-files", async (request, res) => {
-    deleteLocalFiles(getTempFiles());
-    res.status(200).send("All image files have been removed");
-  });
-
-*/
-
+ // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
@@ -78,12 +32,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get("/filteredimage", async (req, res) => {
+    let { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(400).send(`image_url is required`);
+    }
+
+    try {
+      // Filter the image.
+      const filteredpath = await filterImageFromURL(image_url)
+      // Send the resulting file in response.
+      res.status(200).sendFile(filteredpath, {}, (err) => {
+        if (err) { return res.status(422).send(`Not able to process the image`); }
+        // Deleting any used image file.
+        deleteLocalFiles([filteredpath]);
+      })
+    } catch (err) {
+      res.status(422).send(`Not able to process the image, Make sure image url is correct`);
+    }
+  });
+
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("Hey my friend, if you want to filter a public image simply try to add /filteredimage?image_url={{}} to the URL :)")
+    res.send("If you want to filter a public image simply try GET /filteredimage?image_url={{}} :)")
   });
   
 
